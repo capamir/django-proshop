37ee0ef34d008a1a9cd18236bfcd442d7dbd4891
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-10 21:06:05 +0330

    product and review model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Review
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'rating', 'numReviews')
+    raw_id_fields = ('user',)
+    search_fields = ('name',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'rating', 'created')
+    raw_id_fields = ('user', 'product')
+    search_fields = ('name',)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from users.models import User
+import uuid
 # Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # image = models.ImageField(null=True, blank=True,
+    #                           default='/placeholder.png')
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.rating)
