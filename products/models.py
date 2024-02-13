344c3f4d7986a17ce2067d9994df66056a6f86a4
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-13 21:05:05 +0330

    media files is ready

--- products/models.py
+++ products/models.py
@@ -5,8 +5,8 @@ import uuid
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    # image = models.ImageField(null=True, blank=True,
-    #                           default='/placeholder.png')
+    image = models.ImageField(null=True, blank=True,
+                              default='/placeholder.png')
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
