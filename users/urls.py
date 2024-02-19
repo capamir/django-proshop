c62f6f97c5cbc7b8d2323b2799459a235db32cae
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-19 20:13:10 +0330

    user get profile

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.getUsers, name="get_users"),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', views.getUserProfile, name='get_profile'),
+    path('<str:pk>/', views.getUserById, name='get_user'),
+
 ]
